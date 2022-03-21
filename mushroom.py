387218858e0ff72acd89f599a0af6e3d2da10e50
emmraw <101661775+emmraw@users.noreply.github.com>
2022-03-21 16:46:30 +0000

    Update mushroom.py

--- mushroom.py
+++ mushroom.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+
 st.title('Mushroom')
 
 df = pd.read_csv('df_mush.csv')
@@ -7,4 +9,13 @@ df = pd.read_csv('df_mush.csv')
 check = st.checkbox("Tick to see data")
 
 if check:
-  df
+  df.head()
+  
+st.write("Here's our first attempt at importing a data table:")
+
+click = st.button("You can click this button to see the ratio of poisonous mushrooms to edible")
+
+if click:
+  px.pie(values=df['class'])
+  
+  
