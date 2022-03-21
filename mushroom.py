4e0cef913962f45b66b5e7cfd017de0b0eeb9d23
emmraw <101661775+emmraw@users.noreply.github.com>
2022-03-21 17:22:22 +0000

    Update mushroom.py

--- mushroom.py
+++ mushroom.py
@@ -15,6 +15,6 @@ st.write("Here's our first attempt at importing a data table:")
 
 #click = st.button("You can click this button to see the ratio of poisonous mushrooms to edible")
 
-fig = px.pie(df['class'])
+fig = px.pie(values = df['class'])
 st.plotly_chart(fig)
   
