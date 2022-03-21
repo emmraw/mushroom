719c3093e0a1f09158f6663d3b17ee4682acb3cb
emmraw <101661775+emmraw@users.noreply.github.com>
2022-03-21 18:14:19 +0000

    Update mushroom.py

--- mushroom.py
+++ mushroom.py
@@ -16,6 +16,14 @@ st.write("Here's our first attempt at importing a data table:")
 click = st.button("You can click this button to see the ratio of poisonous mushrooms to edible")
 
 if click:
-  fig = px.pie(values = df['class'].value_counts(), names = ['poisonous','edible'])
+  fig = px.pie(values = df['class'].value_counts(), names = ['edible','poisonous'])  # make sure the labels are the right way round
   st.plotly_chart(fig)
   
+option = st.selectbox(
+     'which feature would you like to look at?',
+     ('odor', 'population', 'habitat'))
+
+st.write('You selected:', option)  
+
+fig = px.bar(df, x = option, palette = 'summer', order = df[option].value_counts().index)
+st.plotly_chart(fig)
